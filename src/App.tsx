797a6dee0aaa54c5efb9028fa56a80ce86007fb2
saiganesh71362 <saiganesh71362@gmail.com>
2024-06-02 18:16:50 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap-icons/font/bootstrap-icons.css";
+function App() {
+  return (
+    <>
+      <div>
+        <h1>This Is Sai Ganesh</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
